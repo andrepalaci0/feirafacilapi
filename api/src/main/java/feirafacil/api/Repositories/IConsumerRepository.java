645c4de68d07f0f610d382877abcc7311dfda675
Andre Tivo <andrepalacio14@gmail.com>
2024-11-11 23:02:29 -0300

    feat: add readme

--- api/src/main/java/feirafacil/api/Repositories/IConsumerRepository.java
+++ api/src/main/java/feirafacil/api/Repositories/IConsumerRepository.java
@@ -3,7 +3,5 @@ package feirafacil.api.Repositories;
 import feirafacil.api.Domain.Entities.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface IConsumerRepository extends JpaRepository<Consumer, Long> {
 }
